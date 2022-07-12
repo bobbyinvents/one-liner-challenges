c2f7af3bbd773480a5b01ff2e33eb1fe64af8fea
bobbyinvents <72687709+bobbyinvents@users.noreply.github.com>
2022-07-12 05:19:06 +0200

    Clean code

--- beginner-codes/495/solution.py
+++ beginner-codes/495/solution.py
@@ -2,10 +2,6 @@ from __future__ import annotations
 from beginnercodes.challenges import test
 
 
-def number_split(number: int) -> list[int]:
-    return []  # Put your code here!!!
-
-
 number_split = lambda x: [[a := x // 2] * 2, sorted([a, a + 1])][x % 2]
 
 test(495, number_split)  # Tell it which challenge to test against
